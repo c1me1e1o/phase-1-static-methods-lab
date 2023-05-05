be4d8a2e08c00223d69ef0301ee9f3d1100b29ca
Carlmelo Booc <car1melo@hotmail.com>
2023-05-05 16:55:46 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,3 +1,25 @@
 class Formatter {
   //add static methods here
+  static capitalize(str) {
+    return str.charAt(0).toUpperCase() + str.slice(1);
+
+  }
+  static sanitize(str) {
+    return str.replace(/[^A-Za-z0-9-' ]+/g, '');
+  }
+  static titleize(str) {
+    const exceptions = ['the', 'a', 'an', 'but', 'of', 'and', 'for', 'at', 'by', 'from'];
+    const words = str.split(' ');
+    const titleizedWords = words.map((word, index) => {
+      if (index === 0) {
+        return this.capitalize(word);
+      } else if (exceptions.includes(word)) {
+        return word;
+      } else {
+        return this.capitalize(word);
+      }
+    });
+    return titleizedWords.join(' ');
+    
+  }
 }
